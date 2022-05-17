b68ec81de9fd5119b100d9f371912ef2a79502d1
Thomas Ravache <thomasravache31@gmail.com>
2022-05-17 18:11:25 -0300

    feat: video #02

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Site em React</h1>
+      <p>App em React</p>
+    </div>
+  );
+}
+
+export default App;
