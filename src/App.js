2bca69a4c80e00521986906973a0ca0fa55cef2a
Thomas Ravache <thomasravache31@gmail.com>
2022-05-17 18:44:52 -0300

    feat: conceitos fundamentais do JSX e como utilizar no html

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Thomas';
+
+  const sum = (a, b) => a + b;
+
+  const url = 'https://via.placeholder.com/150';
+
   return (
     <div className="App">
       <h1>Site em React</h1>
-      <p>App em React</p>
+      <p>Olá, {name}</p>
+      <p>{name.toUpperCase()}</p>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
