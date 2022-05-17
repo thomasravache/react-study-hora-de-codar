881f4773d7ad6396a2390bbcbc28e92e2a23589b
Thomas Ravache <thomasravache31@gmail.com>
2022-05-17 18:54:11 -0300

    feat: adicionando mais um componente

---
+++ src/components/Frase.jsx
@@ -0,0 +1,9 @@
+function Frase() {
+    return(
+        <div>
+            <h2>Minha frase</h2>
+        </div>
+    )
+}
+
+export default Frase;
--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,10 @@
+import Frase from "./Frase";
+
 function HelloWorld() {
     return (
         <div>
             <h1>Meu componente</h1>
+            <Frase />
         </div>
     )
 }
