fb783c3278aaf3cc0ff307f99d8d3849099aea79
Thomas Ravache <thomasravache31@gmail.com>
2022-05-18 17:25:29 -0300

    feat: Utilizando componente com fragment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from  './components/List';
 import './App.css';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         profissao="Desenvolvedor"
         foto={url}
       />
+      <List />
     </div>
   );
 }
