4d5d8c101d56fea08473efe891aefc63fcede850
Thomas Ravache <thomasravache31@gmail.com>
2022-05-18 17:42:53 -0300

    feat: Utilizando defaultProps e propTypes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,21 @@
-function Item({ marca }) {
+import PropTypes from 'prop-types'
+
+function Item({ marca, ano_lancamento }) {
     return(
         <>
-            <li>{marca}</li>
+            <li>{marca} - {ano_lancamento}</li>
         </>
     );
+}
+
+Item.propTypes = {
+    marca: PropTypes.string.isRequired,
+    ano_lancamento: PropTypes.number.isRequired,
+}
+
+Item.defaultProps = {
+    marca: 'Falta a marca',
+    ano_lancamento: 0,
 }
 
 export default Item;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,9 +5,10 @@ function List() {
         <>
             <h1>Minha lista</h1>
             <ul>
-                <Item marca="Ferrari" />
-                <Item marca="Fiat" />
-                <Item marca="Peugeot" />
+                <Item marca="Ferrari" ano_lancamento={1985} />
+                <Item marca="Fiat" ano_lancamento={1999} />
+                <Item marca="Peugeot" ano_lancamento={2000} />
+                <Item />
             </ul>
         </>
     )
